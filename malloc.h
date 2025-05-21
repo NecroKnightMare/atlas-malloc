dce1178acc6bd24d6cf20b0bb1fe3dd6c54ed1bf
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-20 19:48:33 -0500

    first file is compiled. Created TD, size for page, tasted and created text file.

--- malloc.h
+++ malloc.h
@@ -5,6 +5,15 @@
 # include <stdio.h>
 # include <string.h>
 
+/*allocate alignment for pages size*/
+#define ALIGN(size, alignment) (((size) + (alignment - 1)) & ~(alignment - 1))
+
+typedef struct heap_size
+{
+    size_t size; /*needed for allocation*/
+    // struct heap_size *next; /*next memory allocation*/
+} heap_t;
+
 
 void *naive_malloc(size_t size); /*task 0*/
 // void *_malloc(size_t size); /*task 1*/
---
+++ naive_malloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include "malloc.h"
+
+void *naive_malloc(size_t size)
+{
+    size_t pagesize = sysconf(_SC_PAGESIZE);
+    size_t total_size = ALIGN(size + sizeof(heap_t), pagesize);
+    void *ptr = sbrk(total_size);
+
+    if (ptr == (void *)-1)
+        return NULL;
+
+    /* Store metadata in heap header */
+    heap_t *header = (heap_t *)ptr;
+    header->size = total_size;
+
+    /* Return pointer to user memory (after heap header) */
+    return (void *)(header + 1);
+}
+
+int main(void)
+{
+    void *ptr = naive_malloc(100);
+    printf("Allocated memory at: %p\n", ptr);
+    return 0;
+}
