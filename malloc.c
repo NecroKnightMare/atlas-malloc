91907e6dcd789db19a861ea5da14fa065f254ade
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-22 09:54:35 -0500

    Betty

--- malloc.c
+++ malloc.c
@@ -15,7 +15,6 @@
 
 void *_malloc(size_t size)
 {
-	
     size_t pagesize = sysconf(_SC_PAGESIZE);
 	size_t total_size = ALIGN(size + sizeof(heap_t), pagesize);
 	void *ptr = sbrk(total_size);
@@ -27,7 +26,6 @@ void *_malloc(size_t size)
 	/* Store metadata in heap header */
 	heap_t *header = (heap_t *)ptr;
     header->size = total_size;
-
 	/* Return pointer to user memory (after heap header) */
 	/*return ((void *)(header + 1));*/
 	return ((void *)((char *)header + sizeof(heap_t)));
