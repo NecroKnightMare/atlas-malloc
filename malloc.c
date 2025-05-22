d6ed61da0660ce4442895ad75b942953d5cc295a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-22 09:40:24 -0500

    commented out main

--- malloc.c
+++ malloc.c
@@ -32,10 +32,10 @@ void *naive_malloc(size_t size)
 	return (void *)((char *)header + sizeof(heap_t));
 }
 
-int main(void)
-{
-    void *ptr = naive_malloc(100);
-    printf("Allocated memory at: %p\n", ptr);
-    return 0;
-}
+// int main(void)
+// {
+//     void *ptr = naive_malloc(100);
+//     printf("Allocated memory at: %p\n", ptr);
+//     return 0;
+// }
 
