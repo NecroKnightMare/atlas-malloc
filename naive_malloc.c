669ebc3b572262118504936c0ccb090ab73b4a7f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-29 08:33:21 -0500

    got the order mixed up

--- naive_malloc.c
+++ naive_malloc.c
@@ -10,48 +10,48 @@
 /**
  * _header - set chunk headers
  * @chunk: pointer to start of chunk
- * @chunk_size: size of chunk
- * @excess_mem: pointer to variable tracking excess memory
+ * @chunk_size: chunk size
+ * @overflow: pointer to excess memory
  */
-void _header(char *chunk, size_t chunk_size, size_t *excess_mem)
+void _header(char *chunk, size_t chunk_size, size_t *overflow)
 {
-	*excess_mem -= chunk_size;
+	*overflow -= chunk_size;
 	*(size_t *)chunk = chunk_size;
-	*(size_t *)(chunk + *(size_t *)chunk) = *excess_mem;
+	*(size_t *)(chunk + *(size_t *)chunk) = *overflow;
 }
 
 /**
- * _unused - find unused chunk block
+ * empty_space - find unused chunk block
  * @heap_start: pointer to heap start
- * @call_nb: specifies how many times malloc has been called
+ * @call_count: specifies how many times malloc has been called
  *
  * Return: pointer to start of unused chunk
  */
-void *_unused(char *heap_start, size_t call_nb)
+void *empty_space(char *heap_start, size_t call_count)
 {
-	while (call_nb > 0)
+	while (call_count > 0)
 	{
 		heap_start += *(size_t *)heap_start;
-		--call_nb;
+		--call_count;
 	}
 	return (heap_start);
 }
 
 /**
- * extend - calls sbrk with size aligned to page size
+ * req_memory - calls sbrk with size aligned to page size
  * @chunk_size: aligned size request
- * @excess_mem: pointer to variable tracking excess memory
+ * @overflow: pointer to excess memory
  *
  * Return: pointer to start of new chunk, NULL on failure
  */
-void *extend(size_t chunk_size, size_t *excess_mem)
+void *req_memory(size_t chunk_size, size_t *overflow)
 {
 	void *chunk;
 
 	chunk = sbrk(align(chunk_size, sysconf(_SC_PAGESIZE)));
 	if (chunk == (void *) -1)
 		return (NULL);
-	*excess_mem += align(chunk_size, sysconf(_SC_PAGESIZE));
+	*overflow += align(chunk_size, sysconf(_SC_PAGESIZE));
 	return (chunk);
 }
 
@@ -65,33 +65,33 @@ void *naive_malloc(size_t size)
 {
 	void *chunk;
 	static void *heap_start;
-	static size_t call_nb;
-	size_t hdr_size, chunk_size, excess_mem;
+	static size_t call_count;
+	size_t hdr_size, chunk_size, overflow;
 
 	hdr_size = sizeof(size_t);
 	chunk_size = align(size, sizeof(void *)) + hdr_size;
 	if (!heap_start)
 	{
-		excess_mem = 0;
-		heap_start = chunk = extend(chunk_size, &excess_mem);
+		overflow = 0;
+		heap_start = chunk = req_memory(chunk_size, &overflow);
 		if (!chunk)
 			return (NULL);
 	}
 	else
 	{
-		chunk = _unused(heap_start, call_nb);
-		excess_mem = *(size_t *)chunk;
-		if (excess_mem < chunk_size + hdr_size)
-			if (!extend(chunk_size, &excess_mem))
+		chunk = empty_space(heap_start, call_count);
+		overflow = *(size_t *)chunk;
+		if (overflow < chunk_size + hdr_size)
+			if (!req_memory(chunk_size, &overflow))
 				return (NULL);
 	}
-	_header(chunk, chunk_size, &excess_mem);
-	++call_nb;
+	_header(chunk, chunk_size, &overflow);
+	++call_count;
 	return ((char *)chunk + hdr_size);
 }
-// int main(void)
-// {
-//     void *ptr = naive_malloc(100);
-//     printf("Allocated memory at: %p\n", ptr);
-//     return 0;
-// }
+int main(void)
+{
+    void *ptr = naive_malloc(100);
+    printf("Allocated memory at: %p\n", ptr);
+    return 0;
+}
