2fe39664863e7a7f725ef7abc1420a7e9514bb4e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-28 19:04:58 -0500

    erased previous code that doesn't work for naive malloc

--- naive_malloc.c
+++ naive_malloc.c
@@ -6,33 +6,6 @@
 #include <sys/types.h>
 #include "malloc.h"
 
-// /**
-//  * naive_malloc - Allocates memory using sbrk with alignment
-//  * @size: Size of memory requested
-//  *
-//  * Description: This function allocates memory by extending the program break
-//  * Return: Pointer to allocated memory or NULL on failure
-// **/
-
-// void *naive_malloc(size_t size)
-// {
-// 	size_t pagesize = sysconf(_SC_PAGESIZE);
-// 	size_t total_size = ALIGN(size + sizeof(heap_t), pagesize);
-// 	void *ptr = sbrk(total_size);
-
-// 	if (ptr == (void *)-1)
-// 	{
-// 		return NULL;
-// 	}
-// 	/* Store metadata in heap header */
-// 	heap_t *header = (heap_t *)ptr;
-// 	header->size = total_size;
-
-// 	/* Return pointer to user memory (after heap header) */
-// 	return (void *)((char *)header + sizeof(heap_t));
-// }
-
-
 
 /**
  * set_hdr - set chunk headers
